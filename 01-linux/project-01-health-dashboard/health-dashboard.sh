eff4660506754c1c18e0575fd762b671074fca34
Darshna <darshna.das98@gmail.com>
2026-07-30 21:01:14 +0530

    Add health dashboard version 2

--- 01-linux/project-01-health-dashboard/health-dashboard.sh
+++ 01-linux/project-01-health-dashboard/health-dashboard.sh
@@ -2,12 +2,23 @@
 
 echo "===== HEALTH DASHBOARD ======"
 host_name=$(hostname)
-
+disk_usage=$(df -h)
+memory_usage=$(free -h)
+uptime_info=$(uptime -p)
 current_date_time=$(date)
 
 logged_in_user=$(whoami)
-
+    
 echo "Host name: $host_name"
+echo "=============================="
+echo "Disk Usage:"
+echo "$disk_usage"
+
+echo "Memory Usage:"
+echo "$memory_usage"
+echo "=============================="
+
+echo "Uptime: $uptime_info"
 echo "Date and Time: $current_date_time"
 echo "Current User: $logged_in_user"
 
